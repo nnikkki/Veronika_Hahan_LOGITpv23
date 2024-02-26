dcfe01ca99cb9a517f2b81330d83fbc1440c02ac
opilane <opilane@HP-CZC2349HTZ>
2024-02-26 10:42:07 +0200

    00

---
+++ MinuOmaModul.py
@@ -0,0 +1,23 @@
+def Korrutis(arv1:float,arv2:float,arv3:float,arv4:1.0)->float:
+    """Funktsioon tagastab 4 aevude korrutis, arvud sisestab kasutaja.Vaikimisi arv4=1.0.
+
+    :param float arv1: sisestage nagu parameeter
+    :param float arv2: sisestage nagu parameeter
+    :param float arv3: sisestage nagu parameeter
+    :param float arv4: sisestage nagu parameeter, vaikimisi võrdub ühega
+    :rtype: float
+
+    """
+
+
+def Suurim_element(arvud1:list,arvud2:list):
+   """Funktsioon kuvad ekraanile suurim arv listidest.
+
+   :param list arvud1: Arvude loetelu
+   :param list arvud2: Arvude loetelu
+   
+   """
+   suurim1=max(arvud1)
+   suurim2=max(arvud1)
+   suurim=max(arvud1,arvud2)
+   print(suurim)
---
+++ kasutamineF.py
@@ -0,0 +1,23 @@
+from MinuOmaModul import *
+from random import *
+
+a=[]
+b=[]
+
+for i in range(5):
+    a.append(randint(-20,20))
+for i in range(1,4):
+    arv=int(input(f"{i}. arv"))
+    b.append(arv)
+print(a)
+print(b)
+print
+Suurim_element(a,b)
+
+a=randint(-10,10)
+print("Esimene arv= ",a)
+b=float(input("Teine arv:"))
+kor=Korrutis(a,b,5.5)
+print("Tulemus: ",kor)
+kor=Korrutis(b,a,b,10)
+print(f"Tulemus: {b}*{a}*{b}*10= ",kor)
